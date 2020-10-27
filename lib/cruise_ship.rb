d84f88c318fff1f141cfb2dc5f4b8dbfb9b678a2
Desiree Lerma <desi.lerma96@yahoo.com>
2020-10-26 21:43:43 -0500

    commit tree a26bcc63f1b03d6adeb0917c99d7d07d656fad14

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,12 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = "" # where we insert the winners name
+passengers.each do |suite, name| # use each b/c we don't want to collect the pair, we just want the winners name 
+  if suite == :suite_a && name.start_with?("A") # if suite is suite a and (&&) name starts w/ A
+    winner = name # assign winner to eq. the name that matches both conditions
+  end
+end
+
+winner # call for the winner
 end
